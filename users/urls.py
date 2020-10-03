e38519a99478fe29d53f2e4bc47dd4487c9eccf2
Viraj Solanki <virazssolanki@gmail.com>
2020-10-03 23:44:33 +0530

    pw redet link and form postiona

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('updateprofile/<email>', views.updateprofile, name='updateprofile'),
     path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True, template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='referral/home.html'), name='logout'),
-    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html', html_email_template_name='users/password_reset_html_email.html'), name='password_reset'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
